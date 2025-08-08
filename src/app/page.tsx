128c83efcab64b05009af9a25fa494cae44568fd
Enderson Rufino Amorim <endersonrufino@gmail.com>
2025-08-08 12:59:30 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/commom/category-selector";
+import Footer from "@/components/commom/footer";
 import Header from "@/components/commom/header";
 import ProductsList from "@/components/commom/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 
 const Home = async () => {
@@ -15,6 +18,13 @@ const Home = async () => {
 
   const categories = await db.query.categoryTable.findMany({})
 
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true
+    }
+  })
+
   return (
     <div>
       <Header />
@@ -25,6 +35,7 @@ const Home = async () => {
         </div>
 
         <ProductsList title="Mais vendidos" products={products} />
+
         <div className="px-5">
           <CategorySelector categories={categories} />
         </div>
@@ -32,6 +43,10 @@ const Home = async () => {
         <div className="px-5">
           <Image src="/banner-02.png" alt="Autentico" width={0} height={0} sizes="100vw" className="w-full h-auto" />
         </div>
+
+        <ProductsList title="Novos produtos" products={newlyCreatedProducts} />
+
+        <Footer />
       </div>
     </div>
   );
