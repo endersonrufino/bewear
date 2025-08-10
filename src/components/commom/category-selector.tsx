ef0820e77a65acba95662e6cd49beec6e921b409
Enderson Rufino Amorim <endersonrufino@gmail.com>
2025-08-09 21:09:04 -0300

    feat: add category page

--- src/components/commom/category-selector.tsx
+++ src/components/commom/category-selector.tsx
@@ -1,4 +1,6 @@
 
+import Link from "next/link";
+
 import { categoryTable } from "@/db/schema"
 
 import { Button } from "../ui/button";
@@ -13,7 +15,14 @@ const CategorySelector = ({ categories }: CategoryListProps) => {
         <div className="rounded-3xl p-6 bg-[#F4EFFF]">
             <div className="grid grid-cols-2 gap-3">
                 {categories.map((category) => (
-                    <Button key={category.id} variant="ghost" className="rounded-full bg-white font-semibold text-xs cursor-pointer">{category.name}</Button>
+                    <Button
+                        key={category.id}
+                        variant="ghost"
+                        className="rounded-full bg-white font-semibold text-xs cursor-pointer">
+                        <Link href={`/category/${category.slug}`}>
+                            {category.name}
+                        </Link>
+                    </Button>
                 ))}
             </div>
         </div>
--- src/components/commom/product-list-item.tsx
+++ src/components/commom/product-list-item.tsx
@@ -15,7 +15,13 @@ export default function ProductListItem({ product }: ProductListItemProps) {
 
     return (
         <Link href="/" className="flex flex-col gap-4">
-            <Image src={firstVariant.imageUrl} alt={firstVariant.name} width={200} height={200} className="rounded-3xl" />
+            <Image 
+            src={firstVariant.imageUrl} 
+            alt={firstVariant.name} 
+            sizes="100vw"
+            width={0} 
+            height={0} 
+            className="h-auto w-full rounded-3xl" />
             <div className="flex max-w-[200px] flex-col gap-1">
                 <p className="truncate text-sm font-medium">{product.name}</p>
                 <p className="truncate text-xs text-muted-foreground font-medium">{product.description}</p>
