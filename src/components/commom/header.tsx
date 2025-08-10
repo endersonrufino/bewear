361d7e9cf77204b5e01701e017c78e56565e61be
Enderson Rufino Amorim <endersonrufino@gmail.com>
2025-08-10 19:33:06 -0300

    chore: add cart tables

--- src/components/commom/header.tsx
+++ src/components/commom/header.tsx
@@ -9,6 +9,7 @@ import { authClient } from '@/lib/auth-client';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet';
+import Cart from './cart';
 
 export default function Header() {
     const { data: session } = authClient.useSession();
@@ -19,7 +20,7 @@ export default function Header() {
                 <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
             </Link>
 
-            <div className="flex items-center">
+            <div className="flex items-center gap-2">
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button variant={"outline"} size={"icon"}>
@@ -75,6 +76,7 @@ export default function Header() {
                         </div>
                     </SheetContent>
                 </Sheet>
+                <Cart />
             </div>
         </header>
     )
