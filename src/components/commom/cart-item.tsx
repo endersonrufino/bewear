6b465f29dd7ef4a314ef098e5e7b3c669107f7f5
Enderson Rufino Amorim <endersonrufino@gmail.com>
2025-08-12 18:54:37 -0300

    feat: ensure product can be removed from cart

--- src/components/commom/cart-item.tsx
+++ src/components/commom/cart-item.tsx
@@ -1,22 +1,47 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
+import { removeProductFromCart } from "@/actions/remove-cart-product";
 import { formatCentsToBrl } from "@/helpers/money";
 
 import { Button } from "../ui/button";
 
 interface CartItemProps {
-    id: string;
-    productName: string;
-    productVariantName: string;
-    imageUrl: string;
-    productVariantPriceInCents: number;
-    quantity: number;
+  id: string;
+  productName: string;
+  productVariantName: string;
+  imageUrl: string;
+  productVariantPriceInCents: number;
+  quantity: number;
 }
 
 const CartItem = ({ id, productName, productVariantName, imageUrl, productVariantPriceInCents, quantity }: CartItemProps) => {
-    return (
-         <div className="flex items-center justify-between">
+
+  const queryClient = useQueryClient()
+ 
+  const removeProductFromCartMutation = useMutation({
+    mutationKey: ['remove-cart-product'],
+    mutationFn: () => removeProductFromCart({ cartItemId: id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    }
+  });
+
+  const handleDeleteClick = () => {
+    removeProductFromCartMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Produto removido do carrinho.")
+      },
+      onError: () => {
+        toast.error("Erro ao remover produto do carrinho.")
+      }
+    });
+  }
+
+  return (
+    <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
         <Image
           src={imageUrl}
@@ -31,18 +56,18 @@ const CartItem = ({ id, productName, productVariantName, imageUrl, productVarian
             {productVariantName}
           </p>
           <div className="flex w-[100px] items-center justify-between rounded-lg border p-1">
-            <Button className="h-4 w-4" variant="ghost" onClick={() => {}}>
+            <Button className="h-4 w-4" variant="ghost" onClick={() => { }}>
               <MinusIcon />
             </Button>
             <p className="text-xs font-medium">{quantity}</p>
-            <Button className="h-4 w-4" variant="ghost" onClick={() => {}}>
+            <Button className="h-4 w-4" variant="ghost" onClick={() => { }}>
               <PlusIcon />
             </Button>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-end justify-center gap-2">
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className="cursor-pointer"  onClick={handleDeleteClick}>
           <TrashIcon />
         </Button>
         <p className="text-sm font-bold">
@@ -50,7 +75,7 @@ const CartItem = ({ id, productName, productVariantName, imageUrl, productVarian
         </p>
       </div>
     </div>
-    )
+  )
 }
 
 export default CartItem;
